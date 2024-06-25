67537b1c58d5e259d8ccca3ca68ac8b089a9406f
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-06-25 11:32:13 -0300

    Delete Parciales/Recomendador.java

--- Parciales/Recomendador.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlp.info.oo1.ParcialOO1Debate;
-
-import java.util.List;
-
-public interface Recomendador {
-	
-	public abstract List<Publicacion> filtro(Debate debate, Usuario usuario);
-}
