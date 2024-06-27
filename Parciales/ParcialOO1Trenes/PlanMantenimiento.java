d73da37a9dd2495f0f9d2c27d4fef5be54453ef7
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-06-27 18:21:37 -0300

    Add files via upload

---
+++ Parciales/ParcialOO1Trenes/PlanMantenimiento.java
@@ -0,0 +1,37 @@
+package ar.edu.unlp.info.oo1.ParcialOO1Trenes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanMantenimiento {
+	private String version;
+	private List<Modelo> modelos;
+	private List<Tarea> tareas;
+	
+	public PlanMantenimiento(String version) {
+		super();
+		this.version = version;
+		this.modelos = new ArrayList<>();
+		this.tareas = new ArrayList<>();
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public List<Modelo> getModelos() {
+		return modelos;
+	}
+
+	public List<Tarea> getTareas() {
+		return tareas;
+	}
+	
+	public void agregarTarea(Tarea tarea) {
+		this.tareas.add(tarea);
+	}
+	
+	public void obtenerCostoPlan() {
+		// ANALIZAR POSIBLE SOLUCION
+	}
+}
---
+++ Parciales/ParcialOO1Trenes/Repuesto.java
@@ -0,0 +1,22 @@
+package ar.edu.unlp.info.oo1.ParcialOO1Trenes;
+
+public class Repuesto {
+	private String nombre;
+	private double costo;
+	
+	public Repuesto(String nombre, double costo) {
+		super();
+		this.nombre = nombre;
+		this.costo = costo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getCosto() {
+		return costo;
+	}
+	
+	
+}
