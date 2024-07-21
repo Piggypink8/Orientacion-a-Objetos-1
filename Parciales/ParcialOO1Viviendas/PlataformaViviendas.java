c4d1b33753d7aa0f68b556b8c7c4de2df7b9335d
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-07-21 12:51:06 -0300

    Update methods PlataformaViviendas.java

--- Parciales/ParcialOO1Viviendas/PlataformaViviendas.java
+++ Parciales/ParcialOO1Viviendas/PlataformaViviendas.java
@@ -1,5 +1,27 @@
 package ar.edu.unlp.info.oo1.ParcialOO1Viviendas;
 
-public class PlataformaViviendas {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PlataformaViviendas {
+  private List<Servicio> servicios;
+
+  public PlataformaViviendas() {
+		this.servicios = new ArrayList<>();
+	}
+  
+	public Cliente registrarCliente(String nombre, String direccion) {
+		Cliente cliente = new Cliente(nombre,direccion);
+		return cliente;
+	}
+	
+	public Servicio registrarServicioParquizacion(double precioPorHora, int cantHoras, int cantMaquinas, double costoMantenimiento) {
+		Servicio servicioParquizacion = new ServicioParquizacion(precioPorHora, cantHoras, cantMaquinas, costoMantenimiento);
+		return servicioParquizacion;
+	}
+	
+	public Servicio registrarServicioLimpieza(double precioPorHora, int cantHoras, double tarifaMinima) {
+		Servicio servicioLimpieza = new ServicioLimpieza(precioPorHora, cantHoras, tarifaMinima);
+		return servicioLimpieza;
+	}
 }
