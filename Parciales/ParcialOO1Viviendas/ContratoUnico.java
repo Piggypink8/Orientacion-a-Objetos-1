2c87bd0b89039dc9951237f5b67bc7a372c667d4
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-07-21 13:06:45 -0300

    Add files via upload

---
+++ Parciales/ParcialOO1Viviendas/ContratoUnico.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo1.ParcialOO1Viviendas;
+
+import java.time.LocalDate;
+
+public class ContratoUnico extends Contrato {
+
+	public ContratoUnico(LocalDate fecha, Servicio servicio, Cliente cliente) {
+		super(fecha, servicio, cliente);
+		// TODO Auto-generated constructor stub
+	}
+
+}
