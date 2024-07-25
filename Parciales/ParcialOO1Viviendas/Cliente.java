b5392a984e47ed377aa2c991e136075624bb1997
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-07-25 20:09:45 -0300

    Update Cliente.java

--- Parciales/ParcialOO1Viviendas/Cliente.java
+++ Parciales/ParcialOO1Viviendas/Cliente.java
@@ -1,12 +1,17 @@
 package ar.edu.unlp.info.oo1.ParcialOO1Viviendas;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Cliente {
 	private String nombre;
 	private String direccion;
-	
+	private List<Contrato> serviciosContratados;
+
 	public Cliente(String nombre, String direccion) {
 		this.direccion = direccion;
 		this.nombre = nombre;
+		this.serviciosContratados = new ArrayList<>();
 	}
 
 	public String getNombre() {
@@ -17,4 +22,13 @@ public class Cliente {
 		return direccion;
 	}
 
+	public List<Contrato> getServiciosContratados() {
+		return serviciosContratados;
+	}
+
+	public double obtenerMontoServiciosContratados() {
+		return this.getServiciosContratados().stream()
+				.mapToDouble(contrato -> contrato.obtenerMonto())
+				.sum();
+	}
 }
