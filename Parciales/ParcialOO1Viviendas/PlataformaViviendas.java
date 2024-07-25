9824d0fee62049c4fcadd0dd7cd8c22e5f21fc7a
Piggypink8 <73083105+Piggypink8@users.noreply.github.com>
2024-07-25 20:11:35 -0300

    Update PlataformaViviendas.java

--- Parciales/ParcialOO1Viviendas/PlataformaViviendas.java
+++ Parciales/ParcialOO1Viviendas/PlataformaViviendas.java
@@ -24,4 +24,22 @@ public class PlataformaViviendas {
 		Servicio servicioLimpieza = new ServicioLimpieza(precioPorHora, cantHoras, tarifaMinima);
 		return servicioLimpieza;
 	}
+
+	public Contrato registrarContratoUnico(LocalDate fecha, Servicio servicio, Cliente cliente) {
+		Contrato contratoUnico = new ContratoUnico(fecha, servicio, cliente);
+		return contratoUnico;
+	}
+	
+	public Contrato registrarContratoProlongado(LocalDate fecha, Servicio servicio, Cliente cliente, int cantDias) {
+		Contrato contratoProlongado = new ContratoProlongado(fecha, servicio, cliente, cantDias);
+		return contratoProlongado;
+	}
+	
+	public List<Servicio> getServicios() {
+		return servicios;
+	}
+	
+	public double obtenerMontoAPagarCliente(Cliente cliente) {
+		return cliente.obtenerMontoServiciosContratados();
+	}
 }
